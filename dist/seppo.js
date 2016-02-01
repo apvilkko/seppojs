fc139ec58404f8de1b3c4095a6431cdefe3d1b06
Antti Pekka Vilkko <antti.pekka.vilkko@gofore.com>
2016-02-01 12:56:14 +0200

    api: show and hide

--- dist/seppo.js
+++ dist/seppo.js
@@ -18,27 +18,53 @@
   });
 
   class Seppo {
-    constructor() {}
+    constructor() {
+      this.seppo = null;
+      this.SLIDER_UP = 'seppo-slider-up';
+    }
 
     activate() {
-      console.log("activated!");
       var el = document.createElement('div');
       el.classList.add('seppo-wrap');
       var seppo = document.createElement('div');
       seppo.classList.add('seppo-avatar');
+      seppo.classList.add('seppo-slider');
       el.appendChild(seppo);
       document.body.appendChild(el);
+      this.seppo = seppo;
+      setTimeout(() => {
+        this.show();
+      }, 2000);
+    }
+
+    show() {
+      if (!this.seppo.classList.contains(this.SLIDER_UP)) {
+        this.seppo.classList.toggle(this.SLIDER_UP);
+      }
+    }
+
+    hide() {
+      if (this.seppo.classList.contains(this.SLIDER_UP)) {
+        this.seppo.classList.toggle(this.SLIDER_UP);
+      }
     }
 
   }
 
   let seppoInstance = null;
   var SeppoJS = {
+    el: null,
     activate: function () {
       if (!seppoInstance) {
         seppoInstance = new Seppo();
-        seppoInstance.activate();
+        SeppoJS.el = seppoInstance.activate();
       }
+    },
+    show: () => {
+      return seppoInstance.show();
+    },
+    hide: () => {
+      return seppoInstance.hide();
     }
   };
 
