8dd02cbeb785b11e422e2c55a84fce5e9d4b2d9c
Antti Pekka Vilkko <antti.pekka.vilkko@gofore.com>
2016-02-01 09:37:40 +0200

    use umd

--- dist/seppo.js
+++ dist/seppo.js
@@ -1,41 +1,44 @@
-"use strict";
-
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-var Seppo = function () {
-  function Seppo() {
-    _classCallCheck(this, Seppo);
+(function (global, factory) {
+  if (typeof define === "function" && define.amd) {
+    define(["exports"], factory);
+  } else if (typeof exports !== "undefined") {
+    factory(exports);
+  } else {
+    var mod = {
+      exports: {}
+    };
+    factory(mod.exports);
+    global.index = mod.exports;
   }
+})(this, function (exports) {
+  "use strict";
 
-  _createClass(Seppo, [{
-    key: "activate",
-    value: function activate() {
+  Object.defineProperty(exports, "__esModule", {
+    value: true
+  });
+
+  class Seppo {
+    constructor() {}
+
+    activate() {
       console.log("activated!");
     }
-  }]);
 
-  return Seppo;
-}();
-
-var seppoInstance = null;
-
-var SeppoJS = {
-  activate: function activate() {
-    if (!seppoInstance) {
-      seppoInstance = new Seppo();
-      seppoInstance.activate();
-    }
   }
-};
 
-if (typeof window !== 'undefined') {
-  window.SeppoJS = SeppoJS;
-}
+  let seppoInstance = null;
+  var SeppoJS = {
+    activate: function () {
+      if (!seppoInstance) {
+        seppoInstance = new Seppo();
+        seppoInstance.activate();
+      }
+    }
+  };
 
-exports.default = SeppoJS;
+  if (typeof window !== 'undefined') {
+    window.SeppoJS = SeppoJS;
+  }
+
+  exports.default = SeppoJS;
+});
